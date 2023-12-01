0a1c735a176b305409401677db384b10ffa5fc06
Stefan Kraus <stefan.kraus@ul.com>
2023-12-01 09:51:53 +0100

    01: part2

--- src/01/main.rs
+++ src/01/main.rs
@@ -1,14 +1,51 @@
 use std::io;
 
 fn main() {
+    let converting = [
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
+
     let mut sum = 0;
     for input in io::stdin().lines() {
         let line = input.unwrap();
-        let a: Vec<_> = line.chars().filter(|x| x.is_digit(10)).collect();
-        let first_digit = a[0].to_digit(10).unwrap();
-        let last_digit = a[a.len()-1].to_digit(10).unwrap();
+        let mut pos_numbers = Vec::new();
+        for case in converting {
+            let (phrase, num) = case;
+            let first_pos = line.find(phrase);
+            let last_pos = line.rfind(phrase);
+            match first_pos {
+                Some(t) => pos_numbers.push((t, num)),
+                None => (),
+            }
+            match last_pos {
+                Some(t) => pos_numbers.push((t, num)),
+                None => (),
+            }
+
+        }
+        pos_numbers.sort();
+
+        let first_digit = pos_numbers[0].1;
+        let last_digit = pos_numbers[pos_numbers.len()-1].1;
         let number = first_digit * 10 + last_digit;
         sum += number;
     }
-    println!("{}", sum)
+    println!("{}", sum);
 }
