d836eeccb3e0b91f64c6ce596156b873fcd6adfb
Stefan Kraus <stefan.kraus@ul.com>
2023-12-04 08:58:03 +0100

    04: part2
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

--- src/04/main.rs
+++ src/04/main.rs
@@ -1,12 +1,17 @@
+use std::cmp;
 use std::collections::HashSet;
 use std::convert::TryInto;
 use std::io;
 use std::iter::FromIterator;
 
 fn main() {
+    let lines: Vec<String> = io::stdin().lines().map(|x| x.unwrap()).collect();
+    let mut cards: Vec<_> = lines.iter().map(|x| 1).collect();
+
     let mut sum = 0;
-    for line in io::stdin().lines() {
-        let line = line.unwrap();
+
+    for (i, line) in lines.iter().enumerate() {
+        sum += cards[i];
 
         let (_, numbers) = line.split_once(":").unwrap();
         let (winning_numb_part, my_numbs_part) = numbers.split_once("|").unwrap();
@@ -28,11 +33,8 @@ fn main() {
 
         let number_of_wins = in_both.count();
 
-        if number_of_wins != 0 {
-            let base: i64 = 2;
-            sum += base
-                .checked_pow((number_of_wins - 1).try_into().unwrap())
-                .unwrap()
+        for j in i + 1..cmp::min(i + number_of_wins + 1, cards.len()) {
+            cards[j] += cards[i];
         }
     }
 
