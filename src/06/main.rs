676aa7a56b3c07ed22aa92b1a05bc95909098804
Stefan Kraus <stefan.kraus@ul.com>
2023-12-06 10:42:06 +0100

    06: part2
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

--- src/06/main.rs
+++ src/06/main.rs
@@ -8,28 +8,17 @@ fn main() {
     let _ = io::stdin().read_line(&mut time);
     _ = io::stdin().read_line(&mut distance);
 
-    let mut timestrings = time.split_whitespace().collect::<Vec<_>>();
-    let mut distancestrings = distance.split_whitespace().collect::<Vec<_>>();
-    timestrings.remove(0);
-    distancestrings.remove(0);
+    let time_str = time.split_once(" ").unwrap().1.replace(" ", "");
+    let time = time_str.trim().parse::<u64>().unwrap();
+    let distance_str = distance.split_once(" ").unwrap().1.replace(" ", "");
+    let distance = distance_str.trim().parse::<u64>().unwrap();
 
-    let times: Vec<_> = timestrings.into_iter().map(|x| x.parse::<i32>().unwrap()).collect();
-    let distances: Vec<_> = distancestrings.into_iter().map(|x| x.parse::<i32>().unwrap()).collect();
+    let mut result = 0;
 
-    let mut result = 1;
-    for i in 0..times.len() {
-        let t = times[i];
-        let d = distances[i];
-
-        let mut num = 0;
-        for j in 0..t+1 {
-            if (t - j) * j > d {
-                num += 1;
-            }
+    for j in 0..time+1 {
+        if (time - j) * j > distance {
+            result += 1;
         }
-        println!("{num}");
-
-        result *= num;
     }
 
     println!("{result}")
