ab931fe57b9deed68d2dd2e11725a9ee440834c4
Stefan Kraus <stefan.kraus@ul.com>
2023-12-09 10:20:13 +0100

    09: part1
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

---
+++ src/09/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+
+fn simulate(numbers: Vec<i32>) -> i32 {
+    let mut res = 0;
+
+    let mut cur = numbers;
+    while cur.len() > 1 {
+        res += cur[cur.len()-1];
+
+        let mut step = vec![];
+        for i in 0..cur.len()-1 {
+            let prev = cur[i];
+            let nxt = cur[i+1];
+            step.push(nxt-prev);
+        }
+        cur = step;
+    }
+
+    res
+}
+
+fn main() {
+
+    let mut sum = 0;
+    for line in io::stdin().lines() {
+        let numbers: Vec<_> = line.unwrap().split(' ').map(|x| x.parse::<i32>().unwrap()).collect();
+
+        let res = simulate(numbers);
+
+        sum += res;
+    }
+
+    println!("{sum}");
+}
+
+
+
