4d8f507e3bc5484bc5652fbeec91f6cfd4d10dcb
Stefan Kraus <stefan.kraus@ul.com>
2023-12-21 13:59:06 +0100

    21: part2
    Signed-off-by: Stefan Kraus <stefan.kraus@ul.com>

--- src/day21/src/main.rs
+++ src/day21/src/main.rs
@@ -20,31 +20,36 @@ fn main() {
         };
     }
 
-    for _ in 0..64 {
+    let mut numbers = vec![];
+
+    for c in 0..1020 {
         let mut new_positions = HashSet::new();
 
         for p in positions {
             for d in DIRS {
-                let nx = p.0 + d.0;
-                let ny = p.1 + d.1;
+                let nx = (p.0 + d.0);
+                let ny = (p.1 + d.1);
 
-                if !(nx >= 0
-                    && nx < field.len() as isize
-                    && ny >= 0
-                    && ny < field[0].len() as isize)
-                {
-                    continue;
-                }
+                let mx = field.len() as isize;
+                let my = field[0].len() as isize;
 
-                let char_at = field[nx as usize][ny as usize];
+                let access_pos_x = (((nx % mx) + mx) % mx) as usize;
+                let access_pos_y = (((ny % my) + my) % my) as usize;
+
+                let char_at = field[access_pos_x][access_pos_y];
                 if char_at == '.' || char_at == 'S' {
                     new_positions.insert((nx, ny));
                 }
             }
         }
 
+        numbers.push((c + 1, new_positions.len()));
+
         positions = new_positions;
     }
 
-    println!("{}", positions.iter().count());
+    println!("{numbers:#?}");
+
+    // from here on, I took python for some linear regression on F(65), F(65+131), F(65+131*2).
+    println!("{}", 621494544278648);
 }
